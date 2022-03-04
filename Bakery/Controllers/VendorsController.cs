a0e2a04cd65e35ce0128fb87e24e8b51590abe5f
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-03-04 10:44:53 -0800

    add controllers for get /vendors and /vendors/new

---
+++ Bakery/Controllers/VendorsController.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Bakery.Models;
+
+namespace Bakery.Controllers
+{
+  public class VendorsController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+  }
+}
