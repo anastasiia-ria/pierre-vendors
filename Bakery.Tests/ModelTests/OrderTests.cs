9932530590576bc0f0ba5affe9db321aa764ba4b
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-03-04 08:46:44 -0800

    add Bakery.Test starter folders and files

---
+++ Bakery.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+using System.Collections.Generic;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class OrderTests : IDisposable
+  {
+
+  }
+}
---
+++ Bakery.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+using System.Collections.Generic;
+using System;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class VendorTests : IDisposable
+  {
+
+
+  }
+}
